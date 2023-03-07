4947bdd809e9b0eca05ddb5ab5c406b74a9afbab
张振 <zhz@hopesoft.com>
2023-03-07 14:46:26 +0800

    重构代码

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -1,32 +1,46 @@
 package bootstrap
 
 import (
-	"github.com/869413421/wechatbot/handlers"
+	"fmt"
 	"github.com/eatmoreapple/openwechat"
-	"log"
+	"github.com/qingconglaixueit/wechatbot/handlers"
+	"github.com/qingconglaixueit/wechatbot/pkg/logger"
+	"os"
 )
-
-
 
 func Run() {
 	//bot := openwechat.DefaultBot()
 	bot := openwechat.DefaultBot(openwechat.Desktop) // 桌面模式，上面登录不上的可以尝试切换这种模式
 
 	// 注册消息处理函数
-	bot.MessageHandler = handlers.Handler
+	handler, err := handlers.NewHandler()
+	if err != nil {
+		logger.Danger(fmt.Sprintf("handlers.NewHandler error: %v", err))
+		return
+	}
+	bot.MessageHandler = handler
+
 	// 注册登陆二维码回调
 	bot.UUIDCallback = openwechat.PrintlnQrcodeUrl
 
 	// 创建热存储容器对象
 	reloadStorage := openwechat.NewJsonFileHotReloadStorage("storage.json")
+
 	// 执行热登录
-	err := bot.HotLogin(reloadStorage)
+	err = bot.HotLogin(reloadStorage)
 	if err != nil {
-		if err = bot.Login(); err != nil {
-			log.Printf("login error: %v \n", err)
+		if err := os.Remove("storage.json"); err != nil {
+			logger.Warning(fmt.Sprintf("os.Remove storage.json error: %v", err))
+			return
+		}
+		reloadStorage := openwechat.NewJsonFileHotReloadStorage("storage.json")
+		err = bot.HotLogin(reloadStorage)
+		if err != nil {
+			logger.Warning(fmt.Sprintf("bot.HotLogin error: %v", err))
 			return
 		}
 	}
+
 	// 阻塞主goroutine, 直到发生异常或者用户主动退出
-	bot.Block()
+	_ = bot.Block()
 }
